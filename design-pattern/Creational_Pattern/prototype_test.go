5595db0b1fc0c226f0b3ac364e5c4b72d04cffea
Yixuan <fanyixuanf@gmail.com>
2022-10-24 20:36:10 +0800

    prototype

--- design-pattern/Creational_Pattern/prototype_test.go
+++ design-pattern/Creational_Pattern/prototype_test.go
@@ -7,3 +7,56 @@
 @Version: 1.0.0
 */
 package creation_pattern
+
+import (
+	"fmt"
+	"testing"
+)
+
+type clone interface {
+	clone() clone
+}
+
+type prototypeManager struct {
+	prototype map[string]clone
+}
+
+func newPrototypeManager() *prototypeManager {
+	return &prototypeManager{
+		prototype: make(map[string]clone),
+	}
+}
+
+func (p *prototypeManager)get(name string) clone {
+	return p.prototype[name].clone()
+}
+
+func (p *prototypeManager)set(name string, prototype clone) {
+	p.prototype[name] = prototype
+}
+
+type protetype1 struct {
+	name string
+}
+
+func (p *protetype1)clone() clone {
+	pc := *p
+	return &pc
+}
+
+func TestPrototype(t *testing.T){
+	manager := newPrototypeManager()
+	p := &protetype1{name: "prototype"}
+	manager.set("prototype", p)
+	p1 := manager.get("prototype")
+
+	p2 := p1.clone()
+
+	fmt.Println(&p1)
+	fmt.Println(&p2)
+
+	if p1 != p2 {
+		t.Log("p1 != p2")
+	}
+
+}
