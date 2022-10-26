cf24649527a9245e07f994d0e2817fb261d30491
Yixuan <fanyixuanf@gmail.com>
2022-10-26 21:20:29 +0800

    bridge

--- design-pattern/Structural_Pattern/bridge_test.go
+++ design-pattern/Structural_Pattern/bridge_test.go
@@ -8,3 +8,76 @@
 */
 package Structural_Pattern
 
+import (
+	"fmt"
+	"testing"
+)
+
+type AbstractMessage interface {
+	SendMessage(text, to string)
+}
+
+type MessageImplement interface {
+	Send(text, to string)
+}
+
+
+type MessageSMS struct {}
+
+func VisSMS() MessageImplement {
+	return &MessageSMS{}
+}
+
+func (sms *MessageSMS)Send(text, to string) {
+	fmt.Printf("Send %s to %s via sms\n", text, to)
+}
+
+type MessageEmail struct {}
+
+func ViaEmail() MessageImplement {
+	return &MessageEmail{}
+}
+
+func (e *MessageEmail)Send (text, to string) {
+	fmt.Printf("Send %s to %s mail \n", text, to)
+}
+
+type CommonMessage struct {
+	method MessageImplement
+}
+
+func NewCommonMessage(method MessageImplement) *CommonMessage {
+	return &CommonMessage{method: method}
+}
+
+func (c *CommonMessage)SendMessage(text, to string) {
+	c.method.Send(text ,to)
+}
+
+type UrgencyMessage struct {
+	method MessageImplement
+}
+
+func NewUrgencyMessage(method MessageImplement) *UrgencyMessage {
+	return &UrgencyMessage{method: method}
+}
+
+func (u *UrgencyMessage)SendMessage(text, to string) {
+	u.method.Send(text, to)
+}
+
+
+func TestMessage(t *testing.T) {
+	cms := NewCommonMessage(VisSMS())
+	cms.SendMessage("吃了吗", "zhangsan")
+
+	cml := NewCommonMessage(ViaEmail())
+	cml.SendMessage("吃了吗", "zhangsan")
+
+	ums := NewUrgencyMessage(VisSMS())
+	ums.SendMessage("吃了吗", "zhangsan")
+
+	uml := NewUrgencyMessage(ViaEmail())
+	uml.SendMessage("吃了吗", "zhangsan")
+
+}
