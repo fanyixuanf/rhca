d55fd89f1cf7b31cb301ab5841c0e4e77ed5b302
Yixuan <fanyixuanf@gmail.com>
2022-11-05 20:10:52 +0800

    priority queue

--- algorithms/priority-queue/priority-queue.go
+++ algorithms/priority-queue/priority-queue.go
@@ -7,3 +7,63 @@
 @Version: 1.0.0
 */
 package priority_queue
+
+import (
+	"container/heap"
+	"fmt"
+)
+
+type Item struct {
+	value string // 值
+	priority int // 优先级
+	index int // 堆的索引
+}
+
+type PriorityQueue []*Item
+
+func (pq PriorityQueue) Len() int {
+	return len(pq)
+}
+
+func (pq PriorityQueue) Less(i, j int) bool {
+	return pq[i].priority >= pq[j].priority
+}
+
+func (pq PriorityQueue) Swap(i, j int) {
+	// 交换值
+	pq[i], pq[j] = pq[j], pq[i]
+	// 索引不变
+	pq[i].index = i
+	pq[j].index = j
+}
+
+func (pq *PriorityQueue) Push(x interface{}) {
+	n := len(*pq)
+	item := x.(*Item)
+	item.index = n
+	*pq = append(*pq, item)
+}
+
+func (pq *PriorityQueue) Pop () interface{} {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	old[n-1] = nil  // avoid memory leak
+	item.index = -1 // for safety
+	*pq = old[ : n - 1]
+	return item
+}
+
+func (pq *PriorityQueue) update(item *Item, value string, priority int) {
+	item.value = value
+	item.priority = priority
+	heap.Fix(pq, item.index)
+}
+
+// 打印目前数据
+
+func (pq *PriorityQueue) Print(q PriorityQueue) {
+	for i, v := range q {
+		fmt.Printf("index: %d, value: %s, Priority: %d \n", i, v.value, v.priority)
+	}
+}
--- algorithms/priority-queue/priority-queue_test.go
+++ algorithms/priority-queue/priority-queue_test.go
@@ -7,3 +7,49 @@
 @Version: 1.0.0
 */
 package priority_queue
+
+import (
+	"container/heap"
+	"fmt"
+	"testing"
+)
+
+func TestPriorityQueue(t *testing.T) {
+	// 初始化数据
+	items := map[string]int{
+		"A job": 3,
+		"B job": 2,
+		"C job": 4,
+		"D job": 1,
+	}
+	pq := make(PriorityQueue, len(items))
+	i := 0
+	for value, priority := range items {
+		pq[i] = &Item{
+			value:    value,
+			priority: priority,
+			index:    i,
+		}
+		i++
+	}
+
+	// 初始化最大堆
+	heap.Init(&pq)
+	// 添加数据到队列中
+	item := &Item{
+		value:    "E job",
+		priority: 1,
+	}
+	heap.Push(&pq, item)
+	pq.Print(pq)
+	fmt.Println("-------------------")
+	// 更改优先级
+	pq.update(item, item.value, 5)
+	pq.Print(pq)
+	fmt.Println("-------------------")
+	for pq.Len() > 0 {
+		item := heap.Pop(&pq).(*Item)
+		fmt.Printf("%.2d:%s \n", item.priority, item.value)
+	}
+
+}
