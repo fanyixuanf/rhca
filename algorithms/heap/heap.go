9759ec56e9dfe8f47f009634d3a8bbd4aaf0e96d
Yixuan <fanyixuanf@gmail.com>
2022-11-05 19:04:44 +0800

    min heap and max heap

--- algorithms/heap/heap.go
+++ algorithms/heap/heap.go
@@ -7,3 +7,59 @@
 @Version: 1.0.0
 */
 package heap
+
+// min heap
+type IntHeap []int
+
+func (h IntHeap) Len () int {
+	return len(h)
+}
+
+func (h IntHeap) Less(i, j int) bool {
+	return h[i] <= h[j]
+}
+
+func (h IntHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+
+func (h *IntHeap) Push(x interface{}) {
+	*h = append(*h, x.(int))
+}
+
+func (h *IntHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+
+// max heap
+
+type MaxHeap []int
+
+func (m MaxHeap) Len () int {
+	return len(m)
+}
+
+func (m MaxHeap) Less(i, j int) bool {
+	return m[i] >= m[j]
+}
+
+func (m MaxHeap) Swap(i, j int) {
+	m[i], m[j] = m[j], m[i]
+}
+
+func (m *MaxHeap) Push(x interface{}) {
+	*m = append(*m, x.(int))
+}
+
+func (m *MaxHeap) Pop() interface{} {
+	old := *m
+	n := len(old)
+	x := old[n-1]
+	*m = old[0 : n-1]
+	return x
+}
--- algorithms/heap/heap_test.go
+++ algorithms/heap/heap_test.go
@@ -7,3 +7,30 @@
 @Version: 1.0.0
 */
 package heap
+
+import (
+	"container/heap"
+	"fmt"
+	"testing"
+)
+
+func TestMinHeap(t *testing.T) {
+	h := &IntHeap{2,1,5,7,2}
+	heap.Init(h)
+	heap.Push(h, 3)
+	fmt.Printf("min : %d\n", (*h)[0])
+
+	for h.Len() > 0 {
+		fmt.Printf("%d ",heap.Pop(h))
+	}
+}
+
+func TestMaxHeap(t *testing.T) {
+	m := &MaxHeap{2,1,5,7,2}
+	heap.Init(m)
+	heap.Push(m, 3)
+	fmt.Printf("max : %d\n", (*m)[0])
+	for m.Len() > 0 {
+		fmt.Printf("%d ",heap.Pop(m))
+	}
+}
