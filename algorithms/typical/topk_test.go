da826fb78b633e3cd1e9cf1960c2575ba79ce0c3
Yixuan <fanyixuanf@gmail.com>
2022-11-12 22:22:03 +0800

    topk

---
+++ algorithms/typical/topk_test.go
@@ -0,0 +1,22 @@
+/*
+@Time : 2022/11/12 22:18
+@Author : Yuxue.fan<fanyixuanf+go@gmail.com>
+@File : topk
+@Software: GoLand
+@Description:
+@Version: 1.0.0
+*/
+package typical
+
+import (
+	"fmt"
+	"testing"
+)
+
+// 取出 arr{5,8,2,3,6,4,1,7,9,4,6,2}中最大的5个数
+
+func TestTopK1(t *testing.T) {
+	arr := [12]int{5,8,2,3,6,4,1,7,9,4,6,2}
+	fmt.Println(arr)
+
+}
