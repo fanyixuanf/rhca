46e15275cdf64e91f8abba657d0763b8b835bdd9
Yixuan <fanyixuanf@gmail.com>
2022-11-13 16:43:25 +0800

    hash

--- algorithms/hash-table/hash-table_test.go
+++ algorithms/hash-table/hash-table_test.go
@@ -9,6 +9,9 @@
 package hash_table
 
 import (
+	"bytes"
+	"crypto/sha256"
+	"encoding"
 	"fmt"
 	"testing"
 )
@@ -18,4 +21,39 @@ func TestMap(t *testing.T) {
 	hash := make(map[string]string, 3)
 	hash["test"] = "test"
 	fmt.Println(hash)
+}
+
+func TestHash(t *testing.T) {
+
+	input1 := "The tunneling gopher digs downwards, "
+	input2 := "unaware of what he will find."
+
+	first := sha256.New()
+	first.Write([]byte(input1))
+
+	marshaler, ok := first.(encoding.BinaryMarshaler)
+	if !ok {
+		fmt.Println("first does not implement encoding.BinaryMarshaler")
+	}
+	state, err := marshaler.MarshalBinary()
+	if err != nil {
+		fmt.Println("unable to marshal hash:", err)
+	}
+
+	second := sha256.New()
+
+	unmarshaler, ok := second.(encoding.BinaryUnmarshaler)
+	if !ok {
+		fmt.Println("second does not implement encoding.BinaryUnmarshaler")
+	}
+	if err := unmarshaler.UnmarshalBinary(state); err != nil {
+		fmt.Println("unable to unmarshal hash:", err)
+	}
+
+	first.Write([]byte(input2))
+	second.Write([]byte(input2))
+
+	fmt.Printf("%x\n", first.Sum(nil))
+	fmt.Printf("%x\n", second.Sum(nil))
+	fmt.Println(bytes.Equal(first.Sum(nil), second.Sum(nil)))
 }
