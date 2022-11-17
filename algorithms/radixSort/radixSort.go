232fc9dee7d6ba78fdc3cb6a9e22db9433bbea1a
Yixuan <fanyixuanf@gmail.com>
2022-11-17 20:35:34 +0800

    radix sort

--- algorithms/radixSort/radixSort.go
+++ algorithms/radixSort/radixSort.go
@@ -7,3 +7,46 @@
 @Version: 1.0.0
 */
 package radixSort
+
+func RadixSort(arr []int) []int {
+	if len(arr) <= 1 {
+		return arr
+	}
+	// 获取最大值
+	max := arr[0]
+	for _, v := range arr {
+		if max < v {
+			max = v
+		}
+	}
+
+	for bit := 1; bit < max/bit; bit *= 10 {
+		arr = bitSort(arr, bit)
+	}
+
+	return arr
+}
+
+func bitSort(arr []int, bit int) []int {
+	n := len(arr)
+	// 统计各个位相同的数到bitCounts[]中
+	bitCounts := make([]int, 11)
+	for i := 0; i < n; i++ {
+		num := (arr[i] / bit) % 10
+		bitCounts[num]++
+	}
+	for i := 1; i < 10; i++ {
+		bitCounts[i] += bitCounts[i - 1]
+	}
+	tmp := make([]int, 11)
+	for i := n - 1; i >= 0; i-- {
+		num := (arr[i] / bit) % 10
+		tmp[bitCounts[num] - 1] = arr[i]
+		bitCounts[num]--
+	}
+	for i := 0; i < n; i++ {
+		arr[i] = tmp[i]
+	}
+
+	return arr
+}
--- algorithms/radixSort/radixSort_test.go
+++ algorithms/radixSort/radixSort_test.go
@@ -7,3 +7,23 @@
 @Version: 1.0.0
 */
 package radixSort
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestSort1(t *testing.T) {
+	var arr [3][]int
+	myarr := []int{1, 2, 3, 1, 1, 2, 2, 2, 2, 2, 3}
+
+	for i := 0; i < len(myarr); i++ {
+		arr[myarr[i] - 1] = append(arr[myarr[i] - 1], myarr[i])
+	}
+	fmt.Println(arr)
+}
+
+func TestRadixSort(t *testing.T) {
+	arr := []int{1, 2, 3, 1, 1, 2, 2, 2, 2, 2, 3}
+	fmt.Println(RadixSort(arr))
+}
