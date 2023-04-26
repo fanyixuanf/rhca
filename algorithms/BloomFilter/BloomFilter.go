e88654e842ef170969765bf58d513f61f6321591
Yixuan <fanyixuanf@gmail.com>
2023-04-26 22:20:20 +0800

    bloom bilter

--- algorithms/BloomFilter/BloomFilter.go
+++ algorithms/BloomFilter/BloomFilter.go
@@ -7,3 +7,58 @@
 @Version: 1.0.0
 */
 package BloomFilter
+
+import (
+	"hash/fnv"
+	"math"
+)
+
+type BloomFilter struct {
+	size uint
+	bits []bool
+	funcs []func([]byte) uint32
+}
+
+func NewBloomFilter(items uint, falsePositiveRate float64) *BloomFilter {
+	// 计算位数组大小
+	size := math.Ceil(-float64(items) * math.Log(falsePositiveRate) / math.Pow(math.Log(2), 2))
+	// 计算哈希函数数量
+	n := math.Ceil(math.Log(2) * size / float64(items))
+	// 初始化布隆过滤器
+	filter := &BloomFilter{
+		size:  uint(size),
+		bits:  make([]bool, int(size)),
+		funcs: make([]func([]byte) uint32, int(n)),
+	}
+	// 设置哈希函数
+	for i := 0; i < int(n); i++ {
+		filter.funcs[i] = hashFunc(i)
+	}
+	return filter
+}
+
+func (b *BloomFilter) Add(data []byte) {
+	for _, fn := range b.funcs {
+		i := fn(data) % uint32(b.size) // 根据哈希函数计算哈希值
+		b.bits[i] = true // 将对应的位设为 true
+	}
+}
+
+func (b *BloomFilter) Test(data []byte) bool {
+	for _, fn := range b.funcs {
+		i := fn(data) % uint32(b.size) // 根据哈希函数计算哈希值
+		if b.bits[i] == false {
+			return false // 如果对应位为 false，说明元素不存在
+		}
+	}
+	return true
+}
+
+// 哈希函数
+func hashFunc(seed int) func([]byte) uint32 {
+	return func(data []byte) uint32 {
+		h := fnv.New32a()
+		h.Write(data)
+		return uint32(seed) * h.Sum32()
+	}
+}
--- algorithms/BloomFilter/BloomFilter_test.go
+++ algorithms/BloomFilter/BloomFilter_test.go
@@ -7,3 +7,22 @@
 @Version: 1.0.0
 */
 package BloomFilter
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestBloomFilter(t *testing.T) {
+	// 创建布隆过滤器
+	filter := NewBloomFilter(10000, 0.01)
+	// 添加元素
+	filter.Add([]byte("apple"))
+	filter.Add([]byte("banana"))
+	filter.Add([]byte("orange"))
+	// 检查元素是否存在
+	fmt.Println(filter.Test([]byte("apple")))   // true
+	fmt.Println(filter.Test([]byte("banana")))  // true
+	fmt.Println(filter.Test([]byte("orange")))  // true
+	fmt.Println(filter.Test([]byte("pineapple")))  // false
+}
