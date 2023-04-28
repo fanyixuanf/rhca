1559f33d115d9998052a0673fa82598296b50dd5
Yixuan <fanyixuanf@gmail.com>
2023-04-28 21:20:57 +0800

    图算法

--- algorithms/Graph/Graph.go
+++ algorithms/Graph/Graph.go
@@ -7,3 +7,100 @@
 @Version: 1.0.0
 */
 package Graph
+
+import "fmt"
+
+type Graph struct {
+	vertices []*Vertex
+}
+
+type Vertex struct {
+	name     string
+	visited  bool
+	neightbours []*Vertex
+}
+// 添加顶点
+func (g *Graph) AddVertex(v *Vertex) {
+	g.vertices = append(g.vertices, v)
+}
+// 添加边
+func (v *Vertex) AddNeightbour(neighbour *Vertex) {
+	v.neightbours = append(v.neightbours, neighbour)
+}
+// 栈的数据结构
+type Stack struct {
+	items []*Vertex
+}
+// 入栈
+func (s *Stack) Push(v *Vertex) {
+	s.items = append(s.items, v)
+}
+// 出栈
+func (s *Stack) Pop() *Vertex {
+	if len(s.items) == 0 {
+		return nil
+	}
+	item := s.items[len(s.items)-1]
+	s.items = s.items[:len(s.items)-1]
+	return item
+}
+// 判断栈是否为空
+func (s *Stack) IsEmpty() bool {
+	return len(s.items) == 0
+}
+// 图的深度优先遍历
+func dfs(g *Graph, start *Vertex) {
+	stack := &Stack{}
+	stack.Push(start)
+	for !stack.IsEmpty() {
+		currentVertex := stack.Pop()
+		if !currentVertex.visited {
+			fmt.Printf("%s ", currentVertex.name)
+			currentVertex.visited = true
+		}
+		for _, neighbour := range currentVertex.neightbours {
+			if !neighbour.visited {
+				stack.Push(neighbour)
+			}
+		}
+	}
+}
+
+
+// 队列的数据结构
+type Queue struct {
+	items []*Vertex
+}
+// 入队
+func (q *Queue) Enqueue(v *Vertex) {
+	q.items = append(q.items, v)
+}
+// 出队
+func (q *Queue) Dequeue() *Vertex {
+	if len(q.items) == 0 {
+		return nil
+	}
+	item := q.items[0]
+	q.items = q.items[1:]
+	return item
+}
+// 判断队列是否为空
+func (q *Queue) IsEmpty() bool {
+	return len(q.items) == 0
+}
+// 图的广度优先遍历
+func bfs(g *Graph, start *Vertex) {
+	queue := &Queue{}
+	queue.Enqueue(start)
+	start.visited = true
+	for !queue.IsEmpty() {
+		currentVertex := queue.Dequeue()
+		fmt.Printf("%s ", currentVertex.name)
+		for _, neighbour := range currentVertex.neightbours {
+			if !neighbour.visited {
+				queue.Enqueue(neighbour)
+				neighbour.visited = true
+			}
+		}
+	}
+}
--- algorithms/Graph/Graph_test.go
+++ algorithms/Graph/Graph_test.go
@@ -7,3 +7,38 @@
 @Version: 1.0.0
 */
 package Graph
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestGraph(t *testing.T) {
+	// 创建顶点
+	vA := &Vertex{name: "A"}
+	vB := &Vertex{name: "B"}
+	vC := &Vertex{name: "C"}
+	vD := &Vertex{name: "D"}
+	vE := &Vertex{name: "E"}
+	// 添加边
+	vA.AddNeightbour(vB)
+	vA.AddNeightbour(vD)
+	vB.AddNeightbour(vC)
+	vC.AddNeightbour(vD)
+	vD.AddNeightbour(vE)
+	// 创建图
+	g := &Graph{}
+	g.AddVertex(vA)
+	g.AddVertex(vB)
+	g.AddVertex(vC)
+	g.AddVertex(vD)
+	g.AddVertex(vE)
+	// 深度优先搜索
+	dfs(g, vA)
+
+	vA.visited, vB.visited, vC.visited, vD.visited, vE.visited = false, false, false, false, false
+
+	fmt.Println()
+	// 广度优先
+	bfs(g, vA)
+}
