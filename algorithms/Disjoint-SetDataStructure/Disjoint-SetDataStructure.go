36066b037a45e7146f13c0e9aacc5f9ba52b7c77
Yixuan <fanyixuanf@gmail.com>
2023-04-28 20:48:05 +0800

    并查集

--- algorithms/Disjoint-SetDataStructure/Disjoint-SetDataStructure.go
+++ algorithms/Disjoint-SetDataStructure/Disjoint-SetDataStructure.go
@@ -7,3 +7,39 @@
 @Version: 1.0.0
 */
 package Disjoint_SetDataStructure
+
+type UnionFind struct {
+	parent []int
+	count int
+}
+
+// 初始化, 每个节点创建集合
+func (uf *UnionFind) Init (n int) {
+	uf.parent = make([]int, n)
+	for i := 0; i < n; i++ {
+		uf.parent[i] = i
+	}
+	uf.count = n
+}
+
+// 找到集合所在的根节点
+func (uf *UnionFind) Find(p int) int {
+	for p != uf.parent[p] {
+		uf.parent[p] = uf.parent[uf.parent[p]]
+		p = uf.parent[p]
+	}
+	return p
+}
+
+func (uf *UnionFind) Union(p, q int) {
+	rootP, rootQ := uf.Find(p), uf.Find(q)
+	if rootP == rootQ {
+		return
+	}
+	uf.parent[rootP] = rootQ
+	uf.count--
+}
+
+func (uf *UnionFind) Count() int {
+	return uf.count
+}
--- algorithms/Disjoint-SetDataStructure/Disjoint-SetDataStructure_test.go
+++ algorithms/Disjoint-SetDataStructure/Disjoint-SetDataStructure_test.go
@@ -7,3 +7,24 @@
 @Version: 1.0.0
 */
 package Disjoint_SetDataStructure
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestUnionFind(t *testing.T) {
+	var n, m int
+	fmt.Scan(&n, &m)
+
+	uf := UnionFind{}
+	uf.Init(n)
+
+	for i := 0; i < m; i++ {
+		var p, q int
+		fmt.Scan(&p, &q)
+		uf.Union(p, q)
+	}
+
+	fmt.Println(uf.count, "components")
+}
